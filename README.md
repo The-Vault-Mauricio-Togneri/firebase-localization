4e0211ddd61beba5a0fadbe45c2bece93e64089b
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-08-24 21:33:16 +0200

    Added basic structure

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# firebase-localization
-Firebase localization
+# Firebase localization
---
+++ functions/index.js
@@ -0,0 +1,8 @@
+const functions = require('firebase-functions');
+
+// https://github.com/firebase/functions-samples
+// https://us-central1-app-localization-2f645.cloudfunctions.net/[function]
+exports.test = functions.https.onRequest((request, response) => {
+	response.json({property:123});
+	console.log('this is a sample log')
+});
