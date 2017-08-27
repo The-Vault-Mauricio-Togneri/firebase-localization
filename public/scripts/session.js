605e7b323f7c553bc86ae8b90f5b2aa003c85a89
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-08-27 13:07:02 +0200

    Updated secondary color

---
+++ public/scripts/session.js
@@ -0,0 +1,26 @@
+initApp = function () {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var uid = user.uid;
+			var phoneNumber = user.phoneNumber;
+			var providerData = user.providerData;
+			user.getIdToken().then(function (accessToken) {
+				//document.getElementById('account-details').textContent = JSON.stringify({
+				//	email: email
+				//}, null, '  ');
+			});
+		} else {
+			//document.getElementById('account-details').textContent = 'null';
+		}
+	}, function (error) {
+		console.log(error);
+	});
+};
+
+window.addEventListener('load', function () {
+	initApp()
+});
