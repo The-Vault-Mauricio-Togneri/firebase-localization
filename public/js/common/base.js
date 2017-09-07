c751b9bd241b06cad98aca2aa857f1645692d045
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-09-07 14:30:29 +0200

    Code cleanup

---
+++ public/js/common/base.js
@@ -0,0 +1,91 @@
+"use strict"
+
+const app = angular.module('segmentsApp', [])
+
+app.filter('formatDate', function()
+{
+	return function(input)
+	{
+		const date = moment(input)
+		var formatted = date.format('dddd DD MMMM YYYY HH:mm:ss')
+		const diff = moment().startOf('day').diff(date.startOf('day'), 'days')
+
+		if (diff == 1)
+		{
+			formatted += ' (1 day ago)'
+		}
+		else if (diff > 0)
+		{
+			formatted += ' (' + diff + ' days ago)'
+		}
+
+		return formatted
+	}
+})
+
+app.filter('gravatar', function()
+{
+	return function(input)
+	{
+		return "http://www.gravatar.com/avatar/" + md5(input) + "?s=30"
+	}
+})
+
+function byId(id)
+{
+	return document.getElementById(id)
+}
+
+function displayContent()
+{
+	byId('loading-progress').style.display = 'none'
+	byId('overview-content').style.display = 'block'
+}
+
+function showError(message)
+{
+	const alert = byId('error-message')
+	alert.style.visibility = 'visible'
+	alert.children[0].innerHTML = message
+
+	setTimeout(function()
+	{
+		alert.style.visibility = 'hidden'
+		alert.children[0].innerHTML = ''
+	}, 3000)
+}
+
+function controller(id)
+{
+	return angular.element($('[ng-controller=' + id + ']')).scope()
+}
+
+function dialog(id)
+{
+	return $('[ng-controller=' + id + ']')
+}
+
+function openDialog(id)
+{
+	dialog(id).modal()
+}
+
+function closeDialog(id)
+{
+	dialog(id).modal('hide')
+}
+
+function focus(id)
+{
+	$('#' + id).focus()
+}
+
+function downloadFile(path)
+{
+	const a = document.createElement('A')
+	a.href = path
+	a.download = path.substr(path.lastIndexOf('/') + 1)
+	document.body.appendChild(a)
+	a.click()
+	document.body.removeChild(a)
+}
