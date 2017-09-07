9bd59bf17cba1297746ddecfbf0c28b2f0e477a8
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-09-07 18:32:17 +0200

    Code cleanup

--- public/js/common/firebase.js
+++
@@ -1,9 +0,0 @@
-const config = {
-	apiKey: 'AIzaSyD97ohSqPCr_8O0byCUMdqpPB9rTf-Qgtk',
-	authDomain: 'app-localization-2f645.firebaseapp.com',
-	databaseURL: 'https://app-localization-2f645.firebaseio.com',
-	projectId: 'app-localization-2f645',
-	storageBucket: 'app-localization-2f645.appspot.com',
-	messagingSenderId: '503219174247'
-}
-firebase.initializeApp(config)
