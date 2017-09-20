f19d02180b50f2df816f03231fb4971f5e3ec4b8
Mauricio Togneri <mauricio.togneri@gmail.com>
2017-09-21 00:32:17 +0200

    Updated download script

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HOST_NAME='...' # e.g. localization.firebaseapp.com
-TOKEN='...' # e.g. 1f1536e7-25c9-4062-8072-f7469baf1513
+PROJECT_ID='...'
+TOKEN='...'
 
 declare -A LOCALES
 LOCALES[en_GB]='strings-en-GB.xml'
@@ -11,5 +11,5 @@ LOCALES[it_CH]='strings-it-CH.xml'
 
 for locale in "${!LOCALES[@]}"
 do
-	wget "https://$HOST_NAME/api/export/$locale/android?token=$TOKEN" -O ${LOCALES[$locale]}
+	wget "https://$PROJECT_ID.firebaseapp.com/api/export/$locale/android?token=$TOKEN" -O ${LOCALES[$locale]}
 done
